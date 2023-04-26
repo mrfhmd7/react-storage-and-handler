1c0678420890d4ef795baae38349f8c5a9578139
mrfhmd7 <mrf.hmd7@gmail.com>
2023-04-26 23:41:08 +0600

    cosmetic file added

--- src/components/Cosmetics/Cosmetics.js
+++ src/components/Cosmetics/Cosmetics.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import { add } from '../../utilities/calculate';
-// import add from '../../utilities/calculate';
+// import React from 'react';
+// import { add } from '../../utilities/calculate';
+// // import add from '../../utilities/calculate';
 
 const Cosmetics = () => {
-     const first = 55;
-     const second = 66;
-     const total = add(first, second)
+     // const first = 55;
+     // const second = 66;
+     // const total = add(first, second)
+
+     const cosmetics = [
+          {id:1, name:'Alta', price:100},
+          {id:2, name:'Malta', price:200},
+          {id:3, name:'Palta', price:300},
+          {id:4, name:'Galta', price:400},
+          {id:5, name:'Falta', price:500},
+     ];
      return (
           <div>
                <h1>Welcome to my Cosmetics Shop</h1>
-               <p>Total= {total}</p>
+               {/* <p>Total= {total}</p> */}
+               {
+                    cosmetics.map(cosmetic => console.log(cosmetic))
+               }
           </div>
      );
 };
